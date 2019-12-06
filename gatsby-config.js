f12d9a0648fa93f5e5346d8576507400e3f5a5e4
Filipe Raiz <filiperaiz@gmail.com>
2019-12-06 17:20:19 -0300

    post and coments

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
 	plugins: [
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-styled-components`,
+		// needs to be the first to work with gatsby-remark-images
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `uploads`,
+				path: `${__dirname}/static/assets/img`,
+			},
+		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
@@ -15,6 +23,36 @@ module.exports = {
 				path: `${__dirname}/src/images`,
 			},
 		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `posts`,
+				path: `${__dirname}/posts`,
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: "gatsby-remark-relative-images",
+						options: {
+							name: "uploads",
+						},
+					},
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							maxWidth: 960,
+							linkImagesToOriginal: false,
+						},
+					},
+					`gatsby-remark-lazy-load`,
+					`gatsby-remark-prismjs`,
+				],
+			},
+		},
+
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
 		{
